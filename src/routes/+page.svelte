c6cf3730bbfb113b66f479d81245249c5bd2225f
Eric Miranda <eric@pop-os.localdomain>
2024-01-28 09:01:42 +0530

    shift to sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    async function getAmount() {
+        const res = await fetch('http://localhost:8080')
+        return await res.text();
+    }
+
+</script>
+
+<h1>
+    Welcome to Split It. You owe
+    {#await getAmount()}
+        umm
+    {:then amount}
+        {amount}
+    {/await}
+</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
