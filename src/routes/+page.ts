b9b8d4a58a542c02c6cb765415e9500bdd0a8826
Eric Miranda <eric@pop-os.localdomain>
2024-02-04 12:42:23 +0530

    use new endpoints

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,9 +2,9 @@ import type {PageLoad} from "./$types";
 import { env } from "$env/dynamic/public";
 
 export const load: PageLoad = async () => {
-    const res = await fetch(env.PUBLIC_BACKEND_HOSTNAME);
+    const res = await fetch(env.PUBLIC_BACKEND_HOSTNAME + "/api/accounts/eric");
     return {
         // amount: Number(await res.text())
-        amount: -2
+        amount: await res.text()
     }
 }
