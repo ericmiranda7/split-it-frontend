84cda8aaca4674811c06b317eb5babca80c447bb
Eric Miranda <eric@pop-os.localdomain>
2024-02-05 12:36:47 +0530

    add oauth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,15 +1,25 @@
 <script>
 import {env} from "$env/dynamic/public";
+import { page } from '$app/stores';
+
+const isAuthenticated = $page.url.searchParams.has('token');
+const token = $page.url.searchParams.get('token')
+const name = $page.url.searchParams.get('name')
+console.log('t,n', token, name)
 </script>
 
 <svelte:head>
     <script src="https://accounts.google.com/gsi/client" async></script>
 </svelte:head>
 
+{#if !isAuthenticated}
 <p>Hi, please login</p>
+{:else}
+    <p>Welcome eric</p>
+{/if}
 
 <div id="g_id_onload"
-     data-client_id="643429409908-hsahqtog313f521vriipqj6o8fgl0fva.apps.googleusercontent.com"
+     data-client_id="{env.PUBLIC_GOOGLE_CLIENT_ID}"
      data-context="signin"
      data-ux_mode="popup"
      data-login_uri="{env.PUBLIC_OAUTH_REDIRECT_URL}"
